81f8dc37af9b68947203622ae1967a52d2348fb3
Alysia Brown <alysiacbrown@gmail.com>
2016-06-13 20:06:38 -0700

    Added word scoring.

--- controllers/scrabble.js
+++ controllers/scrabble.js
@@ -11,26 +11,28 @@ var ScrabbleController = {
 
 
   getScore: function(req, res) {
-  //   // GET /scrabble/score - shows a form allowing a user to submit a word for scoring
-    var locals = {}
-    var scrabble = new ScrabbleLib()
-    locals.word_score = 20
-    res.render('scrabble/score', locals)
-
+    res.render('scrabble/score')
   },
-  //
+
   postScore: function(req, res) {
-  //   // POST /scrabble/score - processes the user input and renders either
-  //   // a page showing the user submitted word and it's score or
-  //   // in the case of something it can't score, an error page with an "unscorable word" messagerouter.get('/party', Controller.getParty)
-  //   var locals = {}
+    var locals = {}
+    var word = req.body.title
+    var scrabble = new ScrabbleLib()
+    locals.word = word
+    locals.score = (scrabble.score(word));
+    res.render('scrabble/show', locals)
 },
-  //
+
   getNameScore: function(req, res) {
   //   // GET /scrabble/score/:name - A dynamic route that scores whatever word is passed into the :name portion of the dynamic route. Will render either
   //   // a page showing the user submitted word and it's score or
   //   // in the case of something it can't score, an error page with an "unscorable word" message
   //   var locals = {}
+  var locals = {}
+  var word_hash = req.params
+  var scrabble = new ScrabbleLib()
+  locals.word = scrabble.score(word)
+
   }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,11 @@ router.get('/scrabble/score', ScrabbleController.getScore)
 // // POST /scrabble/score - processes the user input and renders either
 // // a page showing the user submitted word and it's score or
 // // in the case of something it can't score, an error page with an "unscorable word" messagerouter.get('/party', Controller.getParty)
-// router.post('/scrabble/score', ScrabbleController.postScore)
+router.post('/scrabble/show', ScrabbleController.postScore)
 //
 // // GET /scrabble/score/:name - A dynamic route that scores whatever word is passed into the :name portion of the dynamic route. Will render either
 // // a page showing the user submitted word and it's score or
 // // in the case of something it can't score, an error page with an "unscorable word" message
 router.get('/scrabble/score/:name', ScrabbleController.getNameScore)
-//
 
 module.exports = router;
